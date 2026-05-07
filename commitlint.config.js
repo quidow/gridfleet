fe882ac90a92c14ff700f51b037efbe47742e496
quidow <60624018+quidow@users.noreply.github.com>
2026-05-07 19:25:32 +0300

    ci(deps): align dependabot commits with release-please

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,16 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["build", "chore", "ci", "deps", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
+    ],
     "scope-empty": [2, "never"],
     "scope-enum": [
       2,
       "always",
-      ["backend", "agent", "frontend", "testkit", "docker", "ci", "docs", "deps", "deps-dev"],
+      ["backend", "agent", "frontend", "testkit", "docker", "ci", "docs", "deps", "deps-dev", "main"],
     ],
     "subject-min-length": [2, "always", 10],
     "subject-case": [2, "always", "lower-case"],
